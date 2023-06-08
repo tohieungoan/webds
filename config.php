75aba23b4fa30097b2515e41e6d950e952ee5ea0
tohieungoan <ngoanth.22it@vku.udn.vn>
2023-06-08 10:25:13 +0700

    mvnpay

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$servername = "localhost";
+$username = "root";
+$password = "naruto523";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+  
+$vnp_TmnCode = "JP7JDW60"; //Website ID in VNPAY System
+$vnp_HashSecret = "GVPMKMOLBSZEDTQITCTPQAICOYIVQYDU"; //Secret key
+$vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+$vnp_Returnurl = "http://localhost/vnpay_php/vnpay_return.php";
+$vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
+//Config input format
+//Expire
+$startTime = date("YmdHis");
+$expire = date('YmdHis',strtotime('+15 minutes',strtotime($startTime)));
